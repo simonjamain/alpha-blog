fca4736918e77ff2fcb4c687882546206bd9ace6
simon.jamain <jamain.simon@gmail.com>
2016-03-02 16:59:07 +0000

    added edit functionnality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,13 +10,26 @@ class ArticlesController < ApplicationController
            flash[:notice] = 'Article was successfully created'
            redirect_to article_path(@article)
        else
-           #flash[:alert] = @article.errors.full_messages
            render 'new'
        end
    end
    
    def show
        @article = Article.find(params[:id])
+   end
+   
+   def edit
+       @article = Article.find(params[:id])
+   end
+   
+   def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+         flash[:notice] = 'Article was successfully updated'
+         redirect_to article_path(@article)
+      else
+         render 'edit'
+      end
    end
    
    private
