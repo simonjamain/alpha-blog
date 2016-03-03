e93539d2f59cdb27303fbefed4cd409b3f73d729
simon.jamain <jamain.simon@gmail.com>
2016-03-03 10:42:48 +0000

    refactor + fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,22 +19,22 @@ class ArticlesController < ApplicationController
    end
    
    def show
-       @article = Article.find(params[:id])
+       set_article
    end
    
    def destroy
-      @article = Article.find(params[:id])
+      set_article
       @article.destroy
       flash[:notice] = 'Article was successfully deleted'
       redirect_to articles_path
    end
    
    def edit
-       @article = Article.find(params[:id])
+       set_article
    end
    
    def update
-      @article = Article.find(params[:id])
+      set_article
       if @article.update(article_params)
          flash[:notice] = 'Article was successfully updated'
          redirect_to article_path(@article)
@@ -44,6 +44,10 @@ class ArticlesController < ApplicationController
    end
    
    private
+   def set_article
+      @article = Article.find(params[:id])
+   end
+   
    def article_params
        params.require(:article).permit(:title, :description)
    end
