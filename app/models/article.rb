64400f994bbaeafa88f245d0bff811a7c568eb65
simon.jamain <jamain.simon@gmail.com>
2016-04-12 08:36:23 +0000

    added user article assoc

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
+    belongs_to :user
+    
+    validates :user_id, presence: true
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
